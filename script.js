898fdfe12965b71c0a663e871edd0790af77f68c
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-07-22 15:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-//your JS code here. If required.
-// Example Cypress test
-
-describe('Favorite Holiday Destination Page', () => {
-    before(() => {
-        cy.visit('/path-to-your-page.html'); // Adjust the path as needed
-    });
-
-    it('should contain emphasized text in lists', () => {
-        cy.contains('List of medias');
-        cy.get('ol li:first strong').should('be.visible'); // for <strong> tag
-        cy.get('ol li:nth-child(2) em').should('be.visible'); // for <em> tag
-        cy.get('ul li:first strong').should('be.visible'); // for <strong> tag
-        cy.get('ul li:nth-child(2) em').should('be.visible'); // for <em> tag
-    });
-});
+cy.get('ol li:first strong').should('be.visible');
+cy.get('ol li:nth-child(2) em').should('be.visible');
+cy.get('ul li:first strong').should('be.visible');
+cy.get('ul li:nth-child(2) em').should('be.visible');
