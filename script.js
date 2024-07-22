c69eedf7355f93aaafeec693c1aca0d8cbe704b4
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-07-22 15:22:43 +0530

    created a submission

--- script.js
+++
@@ -1,4 +0,0 @@
-cy.get('ol li:first strong').should('be.visible');
-cy.get('ol li:nth-child(2) em').should('be.visible');
-cy.get('ul li:first strong').should('be.visible');
-cy.get('ul li:nth-child(2) em').should('be.visible');
